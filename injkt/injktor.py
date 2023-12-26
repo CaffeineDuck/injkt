1c1780d09035c1b0c47ea8cb3d9b6c2893956ce3
CaffeineDuck <hello@samrid.me>
2023-12-26 15:27:17 +0545

    feat: finalize impl of set/ delete/ clear methods

--- injkt/injktor.py
+++ injkt/injktor.py
@@ -1,7 +1,6 @@
 import logging
 import typing as ty
 from dataclasses import dataclass
-from functools import cached_property
 
 from .bind import Bind
 from .exceptions import AlwaysReinitNotCallable, InterfaceNotFound
@@ -19,20 +18,34 @@ class Config:
 
 class Injktor(metaclass=Singleton):
     _impls_cache: dict[type, ty.Any] = {}
+    _binds_map: dict[type, Bind] = {}
 
     def __init__(self, config: Config | None = None):
         self.config = config or Config(set())
+        self.install_binds(self.config.binds)
         logger.info("Injktor initialized")
 
-    @cached_property
-    def _binds_map(self) -> dict[type, Bind]:
-        return {bind.interface: bind for bind in self.config.binds}
+    def install_binds(self, binds: set[Bind]) -> None:
+        logger.debug(f"Installing {len(binds)} binds")
+        for bind_conf in binds:
+            self.set(bind_conf)
 
     def set(self, bind_conf: Bind) -> None:
-        self.config.binds.add(bind_conf)
+        logger.debug(f"Setting {bind_conf.interface} to {bind_conf.implementation}")
+        self._binds_map[bind_conf.interface] = bind_conf
+        self._impls_cache.pop(bind_conf.interface, None)
+
+    def delete(self, interface: type) -> None:
+        logger.debug(f"Deleting {interface}")
+        self._binds_map.pop(interface, None)
+        self._impls_cache.pop(interface, None)
+
+    def clear(self) -> None:
+        logger.debug("Clearing all binds")
+        self._binds_map.clear()
+        self._impls_cache.clear()
 
     def get(self, interface: type) -> type:
-        logger.debug(f"Getting implementation for {interface}")
         bind_conf = self._binds_map.get(interface)
         if bind_conf is None:
             raise InterfaceNotFound(interface)
@@ -50,7 +63,11 @@ class Injktor(metaclass=Singleton):
             logger.debug(
                 f"Cache miss for implementation of {interface}, initializing..."
             )
-            impl_cls = bind_conf.implementation()
+            impl_cls = (
+                bind_conf.implementation()
+                if callable(bind_conf.implementation)
+                else bind_conf.implementation
+            )
             self._impls_cache[interface] = impl_cls
-        logger.debug(f"Returning cached implementation of {interface}")
+        logger.debug(f"Returning cached implementation of {interface}: {impl_cls}")
         return impl_cls
